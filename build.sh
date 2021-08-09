6327e69eec8cdf161a6367df14040a50c22288e7
KMX <60032845+Lehmaning@users.noreply.github.com>
2021-08-09 13:43:04 +0800

    DON'T USE

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
-# Require: wget, base64
+# Require: wget, base64， xargs, split
 wget https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt -O ./gfwlist.tmp
 cat gfwlist.tmp | base64 -d > gfwalist.txt
 
+desc="[Auto Proxy]\n! Expires: 7d\n! Title: GFWList4LL\n! HomePage: https://github.com/Lehmaning/gfwalist\n"
 wget https://github.com/Loyalsoldier/cn-blocked-domain/raw/release/domains.txt -O ./cn-blocked-domain.tmp
-sed -i "s/……/||/g" cn-blocked-domain.tmp
+sed -i "s/^google.*\..*//g" cn-blocked-domain.tmp
+sed -i "s/^www\.//g" cn-blocked-domain.tmp
+sed -i "s/^/||/g" cn-blocked-domain.tmp
+sed -i "1i/$desc/" cn-blocked-domain.tmp
 
 cat cn-blocked-domain.tmp >> gfwalist.txt
+awk ' !x[$0]++' gfwalist.txt
 rm -r *.tmp
