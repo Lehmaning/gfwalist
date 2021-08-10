4cee5b66284e2e726b77285565506b55a9bd2d6a
KMX <60032845+Lehmaning@users.noreply.github.com>
2021-08-10 05:40:49 +0000

    不要用

--- build.sh
+++ build.sh
@@ -1,14 +1,11 @@
 # Require: wget, base64， xargs, split
-wget https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt -O ./gfwlist.tmp
-cat gfwlist.tmp | base64 -d > gfwalist.txt
+tsocks wget https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt -O ./gfwlist.tmp;
+cat gfwlist.tmp | base64 -d > gfwalist.tmp
+sed -i -e  "s/^google.*\..*//g" -e "1,17d" gfwalist.tmp
 
-desc="[Auto Proxy]\n! Expires: 7d\n! Title: GFWList4LL\n! HomePage: https://github.com/Lehmaning/gfwalist\n"
-wget https://github.com/Loyalsoldier/cn-blocked-domain/raw/release/domains.txt -O ./cn-blocked-domain.tmp
-sed -i "s/^google.*\..*//g" cn-blocked-domain.tmp
-sed -i "s/^www\.//g" cn-blocked-domain.tmp
-sed -i "s/^/||/g" cn-blocked-domain.tmp
-sed -i "1i/$desc/" cn-blocked-domain.tmp
-
-cat cn-blocked-domain.tmp >> gfwalist.txt
-awk ' !x[$0]++' gfwalist.txt
-rm -r *.tmp
+desc="! [Auto Proxy]\n! Expires: 7d\n! Title: GFWList4LL\n! HomePage: https://github.com/Lehmaning/gfwalist\n"
+tsocks wget https://github.com/Loyalsoldier/cn-blocked-domain/raw/release/domains.txt -O ./cn-blocked-domain.tmp
+#sed -i "s/^[a-zA-Z0-9]*\.//g" cn-blocked-domain.tmp
+sed -i "s/^wwww\.//g" cn-blocked-domain.tmp
+sed -i -e "s/^/||/g" -e "1i$desc" cn-blocked-domain.tmp
+awk '!x[$0]++' gfwalist.tmp > gfwalist.txt
